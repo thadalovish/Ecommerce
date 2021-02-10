05c352cd651bb96d7b4061845c5d9f84e69e6eaf
lovishthada <lovish.seemly@gmail.com>
2021-02-10 14:56:05 +0530

    add wishlist and add to cart dynamically

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://static-assets.glossier.com/production/spree/images/attachments/000/003/755/portrait_normal/LashSlick.jpg?1556563261",
-        price: "110"
+        price: 110,
+        totalQuantity: 2
     },
     {
         id: "2",
@@ -17,7 +18,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://static-assets.glossier.com/production/spree/images/attachments/000/002/829/portrait_normal/BoyBrow_0_Default_01_ShopGrid.jpg?1549325864",
-        price: "150"
+        price: 150,
+        totalQuantity: 4
     },
     {
         id: "3",
@@ -27,7 +29,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://static-assets.glossier.com/production/spree/images/attachments/000/003/872/portrait_normal/01_Shopgrid_Brow_Flick.jpg?1565206094",
-        price: "80"
+        price: 80,
+        totalQuantity: 3
     },
     {
         id: "4",
@@ -37,7 +40,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://static-assets.glossier.com/production/spree/images/attachments/000/003/872/portrait_normal/01_Shopgrid_Brow_Flick.jpg?1565206094",
-        price: "210"
+        price: 210,
+        totalQuantity: 5
     },
     {
         id: "5",
@@ -47,7 +51,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://images-na.ssl-images-amazon.com/images/I/218YircFpgL.jpg",
-        price: "275"
+        price: 275,
+        totalQuantity: 3
     },
     {
         id: "6",
@@ -57,7 +62,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://i.pinimg.com/originals/17/b3/bf/17b3bf200e95b5a13f3d1a6657762a79.png",
-        price: "305"
+        price: 305,
+        totalQuantity: 7
     },
     {
         id: "7",
@@ -67,7 +73,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://static-assets.glossier.com/production/spree/images/attachments/000/004/423/portrait_normal/futuredew-soloproduct_682_leftedge_copy.jpg?1569419908=&w=600&q=80",
-        price: "120"
+        price: 120,
+        totalQuantity: 10
     },
     {
         id: "8",
@@ -77,7 +84,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://s1.r29static.com/bin/product/e28/x,85/2236888/image.webp",
-        price: "475"
+        price: 475,
+        totalQuantity: 5
     },
     {
         id: "9",
@@ -87,7 +95,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://static-assets.glossier.com/production/spree/images/attachments/000/003/377/portrait_normal/priming_moisturizer.jpg?1551586998",
-        price: "395"
+        price: 395,
+        totalQuantity: 4
     },
     {
         id: "10",
@@ -97,7 +106,8 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://images-na.ssl-images-amazon.com/images/I/41womR%2BecGL._SL1006_.jpg",
-        price: "390"
+        price: 390,
+        totalQuantity: 5
     },
     {
         id: "11",
@@ -107,10 +117,13 @@ let data = [
         isFavorite: false,
         isAdd: false,
         mainImage: "https://images-na.ssl-images-amazon.com/images/I/31QePlAGRPL.jpg",
-        price: "420"
+        price: 420,
+        totalQuantity: 6
     },
-
 ]
+
+let cartArr = [];
+let favArr = [];
 
 function setData() {
     $('.insert-coloumn').empty();
@@ -127,6 +140,23 @@ function setData() {
     }
 }
 
+function updateCartArray() {
+    cartArr = data.filter(cart => {
+        if (cart.isAdd == true) {
+            cart.quantity = 1
+            return { cart }
+        }
+    });
+}
+
+function updateWishlistArray() {
+    favArr = data.filter(Fav => {
+        if (Fav.isFavorite == true) {
+            return { Fav }
+        }
+    });
+}
+
 setData()
 
 function addToCart(id) {
@@ -139,12 +169,7 @@ function addToCart(id) {
     })
     data = updatedCartArr
     setData()
-    let cartArr = data.filter(cart => {
-        if (cart.isAdd == true) {
-            return { cart }
-        }
-    });
-
+    updateCartArray()
     console.log(data, cartArr)
 }
 
@@ -158,11 +183,46 @@ function addFavorite(id) {
     })
     data = updatedFavoriteArr
     setData()
-    let favArr = data.filter(Fav => {
-        if (Fav.isFavorite == true) {
-            return { Fav }
-        }
-    });
-
+    updateWishlistArray()
     console.log(data, favArr)
+}
+
+function reduceQuantity(id) {
+    console.log(id)
+    var quantityUpdateArray = cartArr.map(cart => {
+        if (cart.id == id && cart.quantity > 1) {
+            return { ...cart, quantity: cart.quantity - 1 }
+        }
+        return cart
+    })
+    cartArr = quantityUpdateArray
+}
+
+function addQuantity(id) {
+    console.log(id)
+    var quantityUpdateArray = cartArr.map(cart => {
+        if (cart.id == id && cart.totalQuantity > cart.quantity) {
+            return { ...cart, quantity: cart.quantity + 1 }
+        }
+        return cart
+    })
+    cartArr = quantityUpdateArray
+}
+
+function setCartData() {
+    $('.checkout-modal-wrapper').empty();
+    $('.total-amount-show-in-cart').empty()
+    let totalCartValue = 0;
+    for (var i = 0; i < cartArr.length; i++) {
+        $('.checkout-modal-wrapper').append("<div class='border w-100 rounded mb-2 mt-2 flex p-4 flex-wrap'><div class='row'><div class='col-md-2'><img src='" + cartArr[i].mainImage + "' class='w-3'></div><div class='col-md-7'><h4 class='text-lg font-medium'>" + cartArr[i].productName + "</h4><p class='text-gray-600 text-xs pl-0'>" + cartArr[i].discription + "</p></div><div class='col-md-3'><h4 class='text-3xl font-medium'>$-" + cartArr[i].price + "</h4></div><div class='col-md-12 mt-2'><div class='row'><div class='col-md-4'><button class='add-to-cart-button w-114'><i class='fas fa-trash'></i>&ensp;Delete </button></div><div class='col-md-8'><div class='row'><div class='col-md-12 mt-md-0 mt-2 text-md-center text-left'><button class='add-to-cart-button w-35 d-inline-block' onclick='reduceQuantity(" + cartArr[i].id + ")'><i class='fas fa-minus-circle'></i></button><p class='d-inline-block mr-2'>" + cartArr[i].quantity + "</p><button class='add-to-cart-button w-35 d-inline-block' onclick='addQuantity(" + cartArr[i].id + ")'><i class='fas fa-plus-circle'></i></button></div></div></div></div></div></div></div>");
+        totalCartValue += cartArr[i].quantity * cartArr[i].price;
+        $('.total-amount-show-in-cart').text(totalCartValue)
+    }
+}
+
+function setWishListData() {
+    $('.wishlist-modal-wrapper').empty();
+    for (var i = 0; i < favArr.length; i++) {
+        $('.wishlist-modal-wrapper').append("<div class='border w-100 rounded mb-2 mt-2 flex p-4 flex-wrap'><div class='row'><div class='col-md-2'><img src='" + favArr[i].mainImage + "' class='w-3'></div><div class='col-md-7'><h4 class='text-lg font-medium'>" + favArr[i].productName + "</h4><p class='text-gray-600 text-xs pl-0'>" + favArr[i].discription + "</p></div><div class='col-md-3'><h4 class='text-3xl font-medium'>$-" + favArr[i].price + "</h4></div><div class='col-md-12 mt-2'><div class='row'><div class='col-6'><button class='add-to-cart-button w-114'><i class='fas fa-trash'></i>&ensp;Delete</button></div><div class='col-6'><button class='add-to-cart-button w-114'><i class='fas fa-plus-circle'></i>&ensp;Add To Cart</button></div></div></div></div></div>");
+    }
 }
